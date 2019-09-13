cd7a3bb4837645b47432d819c2c70053c2a22eb4
thearlman <asaperlman@gmail.com>
2019-09-13 07:36:02 -0400

    Put in images, set random speeds ***need to fix promises!

--- Excercises/exercise1/js/script.js
+++ Excercises/exercise1/js/script.js
@@ -14,6 +14,7 @@ let circleSize = 100;
 let squareX;
 let squareY;
 let squareSize = 100;
+let winner;
 
 
 // preload()
@@ -32,16 +33,22 @@ function preload() {
 function setup() {
   // Create our canvas
   createCanvas(640,640);
+  imageMode(CENTER);
+  //initialize img of steve jobz
+  jobz = loadImage('assets/images/jobz.png')
+  gatez = loadImage('assets/images/gatez.png')
 
-  // Start the circle off screen to the bottom left
-  // We divide the size by two because we're drawing from the center
-  circleX = -circleSize/2;
-  circleY = height + circleSize/2;
 
-  // Start the square off screen to the bottom right
+
+  // Start the Steve off screen to the bottom left
   // We divide the size by two because we're drawing from the center
-  squareX = width + squareSize/2;
-  squareY = height + squareSize/2;
+  jobzX = -circleSize/2;
+  jobzY = height + circleSize/2;
+
+  // Start the Bill off screen to the bottom right
+  // We divide the size by two because we're drawing from the center
+  gatezX = width + squareSize/2;
+  gatezY = height + squareSize/2;
 
   // We'll draw rectangles from the center
   rectMode(CENTER);
@@ -55,22 +62,60 @@ function setup() {
 // Change the circle and square's positions so they move
 // Draw the circle and square on screen
 
+
 function draw() {
+  //The random variables(s) affecting bill & Steve's speed.
+  jobzSpeed = random(4);
+  gatezSpeed = random(5);
   // We don't fill the background so we get a drawing effect
 
-  // Move circle up and to the right
-  circleX += 1;
-  circleY -= 1;
+  //Give transperency to images
+  tint(255,175);
+  //Draw Stevie
+  image(jobz,jobzX+5,jobzY,150,150);
+  //Draw Billy
+  image(gatez,gatezX-5,gatezY,150,150);
+  // Move circle (& steve) up and to the right
+  jobzX += jobzSpeed;//+random(-1,+1);
+  jobzY -= jobzSpeed;
   // Make the circle transparent red
   fill(255,0,0,10);
   // Display the circle
-  ellipse(circleX,circleY,circleSize,circleSize);
+  ellipse(jobzX,jobzY,circleSize,circleSize);
 
-  // Move square up and to the left
-  squareX -= 1;
-  squareY -= 1;
+  // Move square (& bill) up and to the left
+  gatezX -= gatezSpeed;//+random(-1,1);
+  gatezY -= gatezSpeed;
   // Make the square transparent blue
   fill(0,0,255,10);
   // Display the square
-  rect(squareX,squareY,squareSize,squareSize);
+  rect(gatezX,gatezY,squareSize,squareSize);
+
+
+    let jobzWins= new Promise(function(resolve, reject){
+      if(jobzX > 640){
+      resolve("jobz");
+    }
+
+    });
+    let gatezWins= new Promise(function(resolve, reject){
+      if(gatezX < 1){
+      resolve("gatez");
+    }
+    });
+
+    Promise.race([jobzWins, gatezWins])
+      .then((winner)=>{
+        console.log(winner);
+      });
+      // .catch((winner)=>{
+      //   console.log(winner);
+      // });
+
+
+      // background(0);
+      // fill(255,0,0);
+      // noStroke();
+      // textSize(32);
+      // text(winner, 180, height/2);
 }
