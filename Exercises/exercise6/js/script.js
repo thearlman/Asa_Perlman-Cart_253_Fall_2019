4cb60cabde22ece5f2501b945ee91355176658db
thearlman <asaperlman@gmail.com>
2019-10-27 13:22:39 -0400

    Ex6: fixed typo: "backgoun(0)" > "background(0)" in script.js:39

--- Exercises/exercise6/js/script.js
+++ Exercises/exercise6/js/script.js
@@ -36,7 +36,7 @@ function setup() { /////FIXED
 // Handles input, movement, eating, and displaying for the system's objects
 function draw() {
   // Clear the background to black
-  backgroun(0);
+  background(0);
 
   // Handle input for the tiger
 
